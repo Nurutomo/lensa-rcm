399128809af2ec2ef77666e1688670e4cea5875b
tixbig <tixbig@gmail.com>
2024-06-04 04:21:04 +0000

    add more content

--- src/Components/Pages/Info.js
+++ src/Components/Pages/Info.js
@@ -1,5 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Badge from 'react-bootstrap/Badge'
+
 function Info() {
-    return ( <></> );
+    return (
+        <>
+            <p>Anda mencari-cari <b>Pricelist?</b></p>
+            <p>Menanyakan <b>harga dan tarif?</b></p>
+
+            <h5>Tenang Wae!</h5>
+
+            <p>Kami akan mengirimkan tampilan <b>Pricelist</b><br />
+                setelah anda menyampaikan kebutuhan dan keinginan Anda.</p>
+
+            <p>Silahkan hubungi <Badge bg="secondary">
+                <FontAwesomeIcon icon='fa-solid fa-coffee' />
+            </Badge></p>
+        </>
+    );
+
 }
 
 export default Info;
--- src/Components/Pages/Reason.js
+++ src/Components/Pages/Reason.js
@@ -1,7 +1,13 @@
 function Reason() {
     return (<>
-    <p>Kenapa harus Lensa RCM?</p>
-    </> );
+        <p>Kenapa harus Lensa RCM?</p>
+        <ol>
+            <li>Dengan menggunakan jasa kami, anda bisa menghemat waktu.</li>
+            <li>Dioperasikan oleh Santri terlatih dari pra hingga pasca produksi.</li>
+            <li>Kami berikan semua yang terbaik untuk acara. Mulai dari gear, layanan & hingga memperoleh hasil yang terbaik.​</li>
+            <li>Anda bisa mendapatkan hasil terbaik dengan penawaran harga yang menarik.</li>
+        </ol>
+    </>);
 }
 
 export default Reason;
